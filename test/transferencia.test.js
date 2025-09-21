bc1c698f1c998180b8e811d5ebb5d259542c35f0
Geovana <gibson.geovana@gmail.com>
2025-09-21 18:15:05 -0300

    Adicionei testes para Get transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -4,14 +4,15 @@ require('dotenv').config()
 const {obterToken} = require('../helpers/autenticacao')
 const postTransferencias = require ('../fixtures/postTransferencias.json')
 
-describe('transferencias', () => {
-    describe ('POST /transferencias', () => {
-        let token
+describe ('transferencias', () => {
+    let token
         
         beforeEach( async () => {
             token = await obterToken ('julio.lima', '123456')  
         })
 
+    describe ('POST /transferencias', () => {
+       
         it ('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
            const bodyTransferencias = {...postTransferencias}
            
@@ -38,8 +39,36 @@ describe('transferencias', () => {
             .send(bodyTransferencias)
             expect(resposta.status).to.equal(422);
             
-
         })
+    })
+    describe ('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferencia contido no banco de dados quando o ID for válido', async() => {
+            const resposta = await request (process.env.BASE_URL)
+                .get('/transferencias/11')
+                .set('Authorization', `Bearer ${token}`)
+                
+
+                expect(resposta.status).to.equal(200)
+                expect(resposta.body.id).to.equal(11)
+                expect(resposta.body.id).to.be.a('number')
+                expect(resposta.body.conta_origem_id).to.equal(1)
+      
+        })
+    })    
+
+    describe('GET /transferencias/', ( ) => {
+        it('Deve retrornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            const resposta = await request (process.env.BASE_URL)
+            .get('/transferencias?page=1&limit=10')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
+            
+        })
+
+
     })
 
 });
